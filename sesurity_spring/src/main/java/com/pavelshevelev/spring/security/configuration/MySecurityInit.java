18533278625755cf2c19f312eec7e286f0df7473
Pavel Shevelev <shpmaster@yandex.ru>
2022-09-01 01:09:27 +0300

    New Project

--- sesurity_spring/src/main/java/com/pavelshevelev/spring/security/configuration/MySecurityInit.java
+++
@@ -1,6 +0,0 @@
-package com.pavelshevelev.spring.security.configuration;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class MySecurityInit extends AbstractSecurityWebApplicationInitializer {
-}
--- sesurity_spring/target/sesurity_spring/WEB-INF/view/view_for_HR.jsp
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-<h3>HERE you can see all salaries </h3>
-</body>
-</html>
